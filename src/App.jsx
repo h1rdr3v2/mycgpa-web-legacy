0d3ceaae64ceb084cd9818cb9c9ae13e9090e51f
Destiny Ezenwata <ezenwatairoegbu97@gmail.com>
2023-05-19 20:46:24 +0100

    working

--- src/App.jsx
+++ src/App.jsx
@@ -12,18 +12,19 @@ function App() {
 				<span className="fil-right">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
-						viewBox="0 0 24 24"
+						viewBox="0 0 20 20"
 						fill="currentColor"
-						className="w-6 h-6"
+						className="w-5 h-5"
 					>
 						<path
 							fillRule="evenodd"
-							d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm8.25 5.25a.75.75 0 01.75-.75h8.25a.75.75 0 010 1.5H12a.75.75 0 01-.75-.75z"
+							d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm7 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
 							clipRule="evenodd"
 						/>
 					</svg>
 				</span>
 			</div>
+			<div className="actionArea"></div>
 		</>
 	)
 }
