1ea25c3e3811d404b15e9cfc9adb77d110de7fde
Grace Ndimele <gracendimele5@gmail.com>
2023-05-20 11:50:39 +0100

    new

---
+++ src/App.old.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react"
+import Logo from "./assets/logo.png"
+import "./App.css"
+import Courses from "../components/Courses"
+
+function App() {
+	const [count, setCount] = useState(0)
+
+	return (
+		<>
+			<div className="head">
+				<img src={Logo} width={142} height={69} alt="logo" />
+				<span className="fil-right">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 20 20"
+						fill="currentColor"
+						className="w-5 h-5"
+					>
+						<path
+							fillRule="evenodd"
+							d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm7 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+							clipRule="evenodd"
+						/>
+					</svg>
+				</span>
+			</div>
+			<div className="actionArea">
+				<Courses />
+			</div>
+		</>
+	)
+}
+
+export default App
